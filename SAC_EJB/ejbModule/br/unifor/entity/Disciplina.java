758ff45c38ac4ff1c11453c2cbf70d4f71b17ef0
Erick Nilsen Pereira de Souza - DATAPREVCE <erick.nilsen@dataprev.gov.br>
2015-10-16 15:34:36 -0300

    Serialização habilitada

--- SAC_EJB/ejbModule/br/unifor/entity/Disciplina.java
+++ SAC_EJB/ejbModule/br/unifor/entity/Disciplina.java
@@ -1,11 +1,13 @@
 package br.unifor.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Disciplina {
+public class Disciplina implements Serializable {
 
 	@Id
 	@GeneratedValue
